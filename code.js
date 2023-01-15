050023a3cfddc7212f12c7c67382e460aa942460
ilvdrskn <99636754+ilvdrskn@users.noreply.github.com>
2023-01-15 17:09:44 +0300

    finished challenge

--- code.js
+++ code.js
@@ -1,8 +1,7 @@
 window.onload = function() {
     var elem = document.getElementById("label");
     elem.onclick = function() {
-        document.getElementById("profile").classList.toggle("hide");
-        document.getElementById("links").classList.toggle("hide");
-        document.getElementById("name").classList.toggle("dark");
+        document.getElementById("links").classList.toggle("hide")
+        
     }
 }
